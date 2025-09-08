00e2c9937de234d308c632b2c99817b5c8755d3e
M.J lali <diyalali49@gmail.com>
2025-09-08 06:25:14 +0000

    container config done

--- 01_basics/01_variables.js
+++
@@ -1,9 +0,0 @@
-const accountId = 1223333
-let accountEmail= "madiha@gmail.com"
-var accountPasssword = "1243234"
-//accountcity = "jaipur"
-
-//accountId =2  output :Assignment to constant variable.
-
-console.log(accountId);
-console.log("lali");
