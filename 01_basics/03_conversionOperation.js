2c0294999417be95284bcba81d4603ddbc43f7ca
M.J lali <diyalali49@gmail.com>
2025-09-21 11:53:46 +0000

    operation discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score =false
 //console.log(typeof (score));
 //console.log(typeof score);
-let valueInNumber = Number(score)
+let valueInNumber=Number(score)
 //console.log(typeof valueInNumber);
 //console.log( valueInNumber); //NaN
 // "33" => 33
